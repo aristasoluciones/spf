d7196c5157bf6382f97a6a4484a22b379addcd3c
aristasoluciones <isc061990@outlook.com>
2021-01-10 23:51:25 -0600

    der

--- scripts/duplicatePoll.php
+++ scripts/duplicatePoll.php
@@ -1,1 +1,18 @@
 <?php
+ini_set('memory_limit','3G');
+echo phpinfo();
+echo $_SERVER["DOCUMENT_ROOT"];exit;
+if(!$_SERVER["DOCUMENT_ROOT"])
+{
+    $_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__).'/..');
+}
+if($_SERVER['DOCUMENT_ROOT'] != "/var/www/mainplatform/public_html")
+{
+    $docRoot = $_SERVER['DOCUMENT_ROOT']."/spf";
+}
+else
+{
+    $docRoot = $_SERVER['DOCUMENT_ROOT'];
+}
+define('DOC_ROOT', $docRoot);
+
